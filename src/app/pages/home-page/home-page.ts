963b79d70f1f80c42a30f9225a52a2b36ee60ae4
NielsLazaroms <niels.lazaroms@live.nl>
2026-02-09 00:17:45 +0100

    Remove Navbar component from HomePage

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import {Navbar} from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    Navbar
   ],
   templateUrl: './home-page.html',
 })
