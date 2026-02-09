741e963821944d050a5aa2b811d548559a5e628b
NielsLazaroms <niels.lazaroms@live.nl>
2026-02-09 19:07:32 +0100

    fix: tested tailwind styling not working

---
+++ src/app/components/section-title/section-title.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-title.html',
+})
+export class SectionTitle {
+
+}
